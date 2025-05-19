e2d2f248935fe62f0c4f12753a9b9b6046831a01
joceesagar <joshisagarm2@gmail.com>
2025-05-19 20:51:06 +0545

    created some backend routes

---
+++ app/api/articles/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+    const endpoint: string = process.env.AIRTABLE_ARTICLE_ENDPOINT!;
+    const token: string = process.env.AIRTABLE_TOKEN!;
+
+    if (!endpoint || !token) {
+        return NextResponse.json({ error: "Missing Airtable API Configuration" }, { status: 500 })
+    }
+    try {
+        const response = await fetch(endpoint, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token,
+            }
+        });
+        if (!response.ok) {
+            console.error("Airtable API error:", response.status, response.statusText);
+            return NextResponse.json({ error: "Error Fetching the data" }, { status: 500 })
+        }
+        const data = await response.json();
+        return NextResponse.json({ data: data }, { status: 200 })
+
+    } catch (error: any) {
+        console.error("Server-side fetch error:", error);
+        return NextResponse.json({ error: `Server-side Fetching error: ${error.message}` }, { status: 500 })
+
+    }
+}
